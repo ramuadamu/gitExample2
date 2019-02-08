d7e6ebdbd3d01251a2c2c3588b2a1d39435f0487
Ramu <ramu@Pursuits-MacBook-Pro-4.local>
2019-02-08 10:54:59 -0500

    first commit

---
+++ gitExample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitExample2
+//
+//  Created by Ramu on 2/8/19.
+//  Copyright © 2019 Ramu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("working on git")
+    }
+
+
+}
+
